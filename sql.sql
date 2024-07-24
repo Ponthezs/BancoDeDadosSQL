d2301638c3d083714cf3575b8616ca8c757f3cf2
Felipe Pontes <pontesfelipe835@gmail.com>
2024-07-24 09:18:29 -0300

    feat

--- sql.sql
+++ sql.sql
@@ -1,9 +1,4 @@
-CREATE TABLE professores (
-    id_professor INTEGER,
-    celular VARCHAR(14),
-    nome VARCHAR(40),
-    id_disciplina INTEGER,
-    PRIMARY KEY(id_professor),
-    FOREIGN KEY (id_disciplina) 
-	REFERENCES disciplinas(id_disciplina)
-);
+copy disciplinas(id_disciplina, nome, ementa)
+from 'C:\disciplinas.csv'
+delimiter ','
+csv header
